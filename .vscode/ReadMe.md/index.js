75ea140f9e66a8d2233191b481eb613f9027e96e
vupputholla-Adharsh <powerstaradarsh12345@gmail.com>
2024-11-17 15:29:30 -0500

    Add code to find even numbers in an array

---
+++ .vscode/ReadMe.md/index.js
@@ -0,0 +1,8 @@
+// Function to find even numbers
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+  }
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  console.log("Even Numbers:", findEvenNumbers(numbers));
+  
