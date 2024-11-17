7026d01f8acfa29910b1cb4f6fe0d5935886d244
vupputholla-Adharsh <powerstaradarsh12345@gmail.com>
2024-11-17 15:30:15 -0500

    Add code to find odd numbers in an array

--- .vscode/ReadMe.md/index.js
+++ .vscode/ReadMe.md/index.js
@@ -6,3 +6,10 @@ function findEvenNumbers(arr) {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   console.log("Even Numbers:", findEvenNumbers(numbers));
   
+  // Function to find odd numbers
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+  }
+  
+  console.log("Odd Numbers:", findOddNumbers(numbers));
+  
